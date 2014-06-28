07e087ef2ba428415e18d3ffe4cf1636e8506f65
Michael Krausse <michael.krausse@m-krausse.de>
2014-06-28 20:09:09 +0200

    flipping the trues to false and the false to trueths

--- wemu_gol.js
+++ wemu_gol.js
@@ -4,6 +4,8 @@ var wemu_gol = function () {
     var theWidth = 400;
     var theHeight = 400;
 
+    var the_field = null;
+
     function init() {
         field = [
             [0, 0, 0],
@@ -17,8 +19,8 @@ var wemu_gol = function () {
         var display_data = [];
         for (x = 0; x < field.length; x++) {
             for (y = 0; y < field[0].length; y++) {
-                if(field[y][x]) {
-                    display_data.push({"x":x, "y":y})
+                if (field[y][x]) {
+                    display_data.push({"x": x, "y": y})
                 }
             }
         }
@@ -26,9 +28,33 @@ var wemu_gol = function () {
         return display_data;
     }
 
+    function nextGeneration(field) {
+        var result = [];
+        for (y = 0; y < field.length; y++) {
+            result.push([]);
+            for (x = 0; x < field[0].length; x++) {
+                result[y].push(field[y][x]);
+                if (field[y][x]) {
+                    result[y][x] = false;
+                } else {
+                    result[y][x] = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    function next() {
+        if (the_field === null) {
+            the_field = init();
+        } else {
+            the_field = nextGeneration(the_field);
+        }
+
+        return the_field;
+    }
+
     return {
-
-
         draw: function () {
             var x_scale = d3.scale.linear()
                 .domain([0, 4])
@@ -46,7 +72,7 @@ var wemu_gol = function () {
                 .attr("height", theHeight);
 
             svgContainer.selectAll("rect")
-                .data(calculateDisplayData(init()))
+                .data(calculateDisplayData(next()))
                 .enter()
                 .append("rect")
                 .attr("x", function (d) {
