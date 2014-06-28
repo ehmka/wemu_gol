184c8ea5df8c34f96ebddbc47ce3e0841a7286ef
Michael Krausse <michael.krausse@m-krausse.de>
2014-06-28 19:51:43 +0200

    calculate display data directly from field

--- wemu_gol.js
+++ wemu_gol.js
@@ -1,23 +1,41 @@
 var wemu_gol = function () {
-    var jsonCircles = [
-        { "x": 1, "y": 1 },
-        { "x": 1, "y": 2 },
-        { "x": 3, "y": 1 },
-        { "x": 2, "y": 0 }
-    ];
+    var field = [];
 
     var theWidth = 400;
     var theHeight = 400;
 
-    return {
-        draw: function () {
+    function init() {
+        field = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [1, 0, 1]
+        ];
+        return field;
+    }
 
+    function calculateDisplayData(field) {
+        var display_data = [];
+        for (x = 0; x < field.length; x++) {
+            for (y = 0; y < field[0].length; y++) {
+                if(field[y][x]) {
+                    display_data.push({"x":x, "y":y})
+                }
+            }
+        }
+
+        return display_data;
+    }
+
+    return {
+
+
+        draw: function () {
             var x_scale = d3.scale.linear()
                 .domain([0, 4])
                 .range([0, theWidth]);
 
             var y_scale = d3.scale.linear()
-                .domain([0,4])
+                .domain([0, 4])
                 .range([0, theHeight]);
 
 
@@ -28,7 +46,7 @@ var wemu_gol = function () {
                 .attr("height", theHeight);
 
             svgContainer.selectAll("rect")
-                .data(jsonCircles)
+                .data(calculateDisplayData(init()))
                 .enter()
                 .append("rect")
                 .attr("x", function (d) {
